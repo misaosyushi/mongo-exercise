0a4d80f70bf60948ae5943ff65fb13039e7ec1ef
misaosyushi <tujitamisaki@gmail.com>
2022-07-19 08:53:30 +0900

    fix format

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ import json
 import os
 
 from dotenv import load_dotenv
+
 from pokemon_repository import PokemonRepository
 
 load_dotenv()
 
 
 def insert_initial_data(repo):
-    with open('./pokemon.json') as f:
+    with open("./pokemon.json") as f:
         data = json.load(f)
     return repo.insert_many(data)
 
 
-if __name__ == '__main__':
-    repository = PokemonRepository(os.getenv('MONGO_URL'), 'test', 'pokemon')
+if __name__ == "__main__":
+    repository = PokemonRepository(os.getenv("MONGO_URL"), "test", "pokemon")
     # insert_initial_data(repository)
 
     # nameで検索
-    results = repository.find(filter={"name": "フシギダネ"})
+    # results = repository.find(filter={"name": "フシギダネ"})
+    # for result in results:
+    #     print(result)
+
+    # projectionでフィールドを指定、$inで配列に含む文字列を指定
+    results = repository.find(
+        projection={"_id": False, "name": True, "types": True},
+        filter={"types": {"$in": ["みず"]}},
+        limit=10,
+    )
     for result in results:
         print(result)
 
-    # projection
     # skip, limit, sort
     # AND検索
-    # OR検索
     # 部分一致
     # 範囲検索
     # トランザクション https://pymongo.readthedocs.io/en/stable/api/pymongo/client_session.html
--- pokemon_repository.py
+++ pokemon_repository.py
@@ -1,18 +1,19 @@
-from pymongo import MongoClient
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
 
 
 class PokemonRepository:
-
     def __init__(self, mongo_url, db_name, coll_name):
         self.client = MongoClient(mongo_url)
         self.db = self.client[db_name]
         self.collection = self.db.get_collection(coll_name)
 
     def find(self, projection=None, skip=0, limit=0, filter=None, sort=None):
-        return self.collection.find(projection=projection, skip=skip, limit=limit, filter=filter, sort=sort)
+        return self.collection.find(
+            projection=projection, skip=skip, limit=limit, filter=filter, sort=sort
+        )
 
     def insert_one(self, document):
         return self.collection.insert_one(document)
