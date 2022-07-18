d535234da76dfecf2b5d256ee9a053985d61c0b0
misaosyushi <tujitamisaki@gmail.com>
2022-07-18 18:23:18 +0900

    fix reposiotry

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 import json
 import os
 
-from pymongo import MongoClient
 from dotenv import load_dotenv
+from pokemon_repository import PokemonRepository
 
 load_dotenv()
 
 
-class MongoRepository:
-
-    def __init__(self, db_name, coll_name):
-        self.client = MongoClient(os.getenv('MONGO_URL'))
-        self.db = self.client[db_name]
-        self.collection = self.db.get_collection(coll_name)
-
-    def find(self, projection=None, skip=0, limit=0, filter=None, sort=None):
-        return self.collection.find(projection=projection, skip=skip, limit=limit, filter=filter, sort=sort)
-
-    def insert_one(self, document):
-        return self.collection.insert_one(document)
-
-    def insert_many(self, documents):
-        return self.collection.insert_many(documents)
-
-
-def mongo_import(repository: MongoRepository):
+def insert_initial_data(repo):
     with open('./pokemon.json') as f:
         data = json.load(f)
-    return repository.insert_many(data)
+    return repo.insert_many(data)
 
 
 if __name__ == '__main__':
-    repository = MongoRepository('test', 'pokemon')
-    # mongo_import(repository)
+    repository = PokemonRepository(os.getenv('MONGO_URL'), 'test', 'pokemon')
+    # insert_initial_data(repository)
+
+    # nameで検索
     results = repository.find(filter={"name": "フシギダネ"})
     for result in results:
         print(result)
+
+    # projection
+    # skip, limit, sort
+    # AND検索
+    # OR検索
+    # 部分一致
+    # 範囲検索
+    # トランザクション https://pymongo.readthedocs.io/en/stable/api/pymongo/client_session.html
---
+++ pokemon_repository.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class PokemonRepository:
+
+    def __init__(self, mongo_url, db_name, coll_name):
+        self.client = MongoClient(mongo_url)
+        self.db = self.client[db_name]
+        self.collection = self.db.get_collection(coll_name)
+
+    def find(self, projection=None, skip=0, limit=0, filter=None, sort=None):
+        return self.collection.find(projection=projection, skip=skip, limit=limit, filter=filter, sort=sort)
+
+    def insert_one(self, document):
+        return self.collection.insert_one(document)
+
+    def insert_many(self, documents):
+        return self.collection.insert_many(documents)
